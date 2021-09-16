2434ffc4abcf59a4f7526dc16c29c751abb59b9b
jlinick <jlinick@jlinick-debian.home.arpa>
2021-09-16 00:12:38 -0400

    uncomment retrieval

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-#${SCRIPT_DIR}/retrieve_data.py
+${SCRIPT_DIR}/retrieve_data.py
 ${SCRIPT_DIR}/move.py
 ${SCRIPT_DIR}/project_and_crop.sh
 ${SCRIPT_DIR}/mean_and_match.py
